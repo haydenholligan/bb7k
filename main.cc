97da4163a876659ae90db0e1975648e4a647c38d
Jesse Hoek <jessechk@gmail.com>
2014-11-29 03:32:57 -0500

    begin gameplay stuff

--- main.cc
+++ main.cc
@@ -9,6 +9,7 @@
 #include "acBuilding.h"
 #include "gym.h"
 #include "residence.h"
+using namespace std;
 
 void takeInput() {
     
@@ -17,14 +18,21 @@ void takeInput() {
 
 
 int main() {
-  
+
 	cout << "WELCOME TO BB7K!" << endl;
 	cout << "Number of players: ";
 	int n;
 	cin >> n;
-	cout << "THANKS!";
+	Board board(n);
 
+	for (int i = 0; i < 4; i++) {
+		cout << "Player " << i << ", choose a piece: " << endl;
+		string s;
+		cin >> s;
+		board.addPlayer(s.at(0));
+	}
 
+	cout << "DONE FOR NOW" << endl;
 
 
 }
